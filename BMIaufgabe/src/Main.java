e9d40b630b0fecc16c86f714054d5a02dd5d1f09
PinkelPunk <PinkelPunk*******>
2015-02-12 18:18:36 +0100

    repo erstellt

---
+++ BMIaufgabe/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		
+		// BMI= körpergewicht(in kg)/körpergrösse(in meter) zum quadrat
+		int kilo=70;
+		double groesse=1.85;
+		double bmi;
+		
+		bmi=kilo/(groesse*groesse);
+		
+		System.out.println("BMI = " + bmi);
+		
+	}
+
+}
