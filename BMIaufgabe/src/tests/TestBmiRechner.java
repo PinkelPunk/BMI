a3f60cdf47f2fbabb8a1e1192e6bacf536d79ae8
PinkelPunk <PinkelPunk******>
2015-02-13 10:03:22 +0100

    methode testGetGewicht für zweigüberdeckung(C1) fertig

--- BMIaufgabe/src/tests/TestBmiRechner.java
+++ BMIaufgabe/src/tests/TestBmiRechner.java
@@ -1,12 +1,26 @@
 package tests;
 
 import junit.framework.TestCase;
-
 import org.junit.*;
+import bmi.BmiRechner;
 
 public class TestBmiRechner extends TestCase
 {
+	@Test
+	public void testGetGewicht_C1()
+	{
+		BmiRechner bmi=new BmiRechner();
+		
+		assertEquals(12, bmi.getGewicht());
+		
+		assertEquals(17, bmi.getGewicht());
+		
+		assertEquals(21, bmi.getGewicht());
+		
+		assertEquals(28, bmi.getGewicht());
+		
+		assertEquals(40, bmi.getGewicht());
+	}
 	
 	
-
 }
